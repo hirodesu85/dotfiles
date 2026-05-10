7ecfa096742f8109706ee17fc9d1f80c358e4335
hirodesu <hirodesu85@gmail.com>
2026-05-10 17:22:17 +0900

    Fix Neovim rendering bug

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,3 +8,13 @@ require("core.keymap")
 
 -- プラグインマネージャにはlazyを使用
 require("config.lazy")
+
+-- Zellij内でNeovimを使った時に背景がバグるバグのワークアラウンド
+-- もしアプデとかで解消されたら消したい
+vim.api.nvim_create_autocmd({ "BufWinEnter", "WinClosed" }, {
+	callback = function()
+		vim.defer_fn(function()
+			vim.cmd("mode")
+		end, 30)
+	end,
+})
